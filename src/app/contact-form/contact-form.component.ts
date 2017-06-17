bbaa97532ce0fb95c542a35c383dc69fe9096e55
cmoodley <cameron@resafrica.com>
2017-06-17 11:35:47 +0200

    adding in routing

---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./styles.css']
+})
+
+export class contactFormComponent {}
